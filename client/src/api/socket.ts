434cdac2f9648962054c8bf018a18ce869be2ccc
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2022-06-02 10:10:15 -0400

    feat(client-socket): added client socket api

---
+++ client/src/api/socket.ts
@@ -0,0 +1,28 @@
+const ws = new WebSocket("ws://localhost:8080/ws");
+
+const connect = () => {
+  console.log("Attempting to connect to websocket...");
+
+  ws.onopen = () => {
+    console.log("Succesfully connected to localhost:8080.");
+  };
+
+  ws.onclose = (event) => {
+    console.log(`Connection to localhost:8080 closed. event message: ${event}`);
+  };
+
+  ws.onmessage = (msg) => {
+    console.log(`Message received from localhost:8080: ${msg}`);
+  };
+
+  ws.onerror = (err) => {
+    console.log(`Error with socket: ${err}`);
+  };
+};
+
+const sendMessage = (message: string) => {
+  console.log(`Sending following message to localhost:8080: ${message}`);
+  ws.send(message);
+};
+
+export { sendMessage, connect };
