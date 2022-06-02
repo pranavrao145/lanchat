61a3a239caa2dade3c37b00726654b90aeb649f7
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2022-06-02 12:21:34 -0400

    feat(client-socket): implemented sending username to server

--- client/src/components/login/Login.tsx
+++ client/src/components/login/Login.tsx
@@ -1,14 +1,32 @@
 import React from "react";
 import { connect, sendMessage } from "../../api/socket";
 
-class Login extends React.Component {
-  constructor(props: any) {
+interface ILoginProps {}
+interface ILoginState {
+  username: string;
+}
+
+class Login extends React.Component<ILoginProps, ILoginState> {
+  constructor(props: ILoginProps) {
     super(props);
     connect();
+
+    this.state = {
+      username: "",
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  sendTestMessage() {
-    sendMessage("This is a test message.");
+  handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({ username: e.target.value });
+  }
+
+  handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    sendMessage(this.state.username);
   }
 
   render() {
@@ -19,26 +37,26 @@ class Login extends React.Component {
             <div className="columns">
               <div className="column is-one-fifth"></div>
               <div className="column">
-                <div className="box">
-                  <div className="field">
-                    <label className="label">Username</label>
-                    <div className="control">
-                      <input
-                        className="input"
-                        type="text"
-                        placeholder="Enter username"
-                      />
+                <form onSubmit={this.handleSubmit}>
+                  <div className="box">
+                    <div className="field">
+                      <label className="label">Username</label>
+                      <div className="control">
+                        <input
+                          className="input"
+                          type="text"
+                          placeholder="Enter username"
+                          onChange={this.handleChange}
+                        />
+                      </div>
+                    </div>
+                    <div className="field">
+                      <button className="button is-fullwidth is-link">
+                        Submit
+                      </button>
                     </div>
                   </div>
-                  <div className="field">
-                    <button
-                      className="button is-fullwidth is-link"
-                      onClick={this.sendTestMessage}
-                    >
-                      Submit
-                    </button>
-                  </div>
-                </div>
+                </form>
               </div>
               <div className="column is-one-fifth"></div>
             </div>{" "}
