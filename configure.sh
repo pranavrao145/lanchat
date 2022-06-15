c769de7d9294bb52a97b4e2923b63319e2233d5a
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2022-06-15 10:52:45 -0400

    chore(remove-docker): undockerized (must do again later)

--- configure.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SERVER_URL="$(hostname -i | tr -s ' ' | cut -d ' ' -f 1)"
-echo "REACT_APP_SERVER_URL=\"ws://$SERVER_URL:8080/ws\"" > client/.env.local
-
-echo "IMPORTANT: The URL to LANChat on your network is: http://$SERVER_URL:3000."
-
-echo "You can now run docker-compose up to start the app!"
